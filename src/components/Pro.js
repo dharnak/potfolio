703c5fe59a6308136b5d0cd70d87ccf86ff92fdc
dharnak <thangavelraja36@gmail.com>
2025-01-22 15:51:56 +0530

    update1.3

--- src/components/Pro.js
+++ src/components/Pro.js
@@ -5,35 +5,38 @@ export default function Pro() {
   return (
     <div className='pro'>
 
-      <div className=' flex justify-evenly pt-10'>
+      <div className='project1'>
         
       <div className='probox'>
       <img className='Facebook' src={Facebook} alt='../'/>
           <h1 className=' text-center text-white'>facebook login page clone (HTML,CSS)</h1>
+          <button className='projectbut1'>view</button>
       </div>
       <div className='probox'>
       <img className='Facebook' src={Facebook} alt='../'/>
           <h1 className=' text-center text-white'>facebook login page clone (HTML,CSS)</h1>
+          <button className='projectbut1'>view</button>
       </div>
       <div className='probox'>
       <img className='Facebook' src={Facebook} alt='../'/>
           <h1 className=' text-center text-white'>facebook login page clone (HTML,CSS)</h1>
+          <button className='projectbut1'>view</button>
       </div>
-      </div>
-      <div className=' flex justify-evenly pt-10'>
-        
         <div className='probox'>
         <img className='Facebook' src={Facebook} alt='../'/>
             <h1 className=' text-center text-white'>facebook login page clone (HTML,CSS)</h1>
+            <button className='projectbut1'>view</button>
         </div>
         <div className='probox'>
         <img className='Facebook' src={Facebook} alt='../'/>
             <h1 className=' text-center text-white'>facebook login page clone (HTML,CSS)</h1>
+            <button className='projectbut1'>view</button>
         </div>
         <div className='probox'>
         <img className='Facebook' src={Facebook} alt='../'/>
             <h1 className=' text-center text-white'>facebook login page clone (HTML,CSS)</h1>
-        </div>
+            <button className='projectbut1'>view</button>
+         </div>
         </div>
     </div>
   )
